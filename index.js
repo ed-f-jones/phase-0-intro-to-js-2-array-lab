426023c9651274969e03d072e3dcd839689614bd
Edward <ed.f.jones@gmail.com>
2023-03-11 21:03:03 -0500

    completed array work

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+} 
+
+function destructivelyPrependCat() {
+   return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(params) {
+    return cats.shift("Milo");
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+ return cats.slice(1);  
+}
